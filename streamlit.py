5f4b1831be46657894c98273057066cb13a93611
Mohamed Arafath <113768609+MohamedArafath205@users.noreply.github.com>
2023-01-15 20:13:04 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,12 +9,25 @@ model = pickle.load(open('model.sav', 'rb'))
 # Title of the page
 st.title('Rock or Mine Prediction')
 
-input_data = st.text_input("Enter the values ")
 
-import numpy as np
-input_data_array = np.asarray(input_data)
-input = input_data_array.reshape(1,-1)
-if st.button('Prediction'):
-    prediction = model.predict(input)
+# Get input array from user
+input_array = st.text_input("Enter the elements of the array, separated by commas: ")
+
+# Convert input string to list of integers
+input_list = [float(x) for x in input_array.split(",")]
+
+# Convert list to numpy array
+input_np = np.array(input_list)
+
+# Reshape array to (1,-1) format
+output_np = input_np.reshape(1,-1)
+
+if st.button('Predict'):
+    model_predict = model.predict(output_np)
+    
+    if(model_predict[0] == 'M'):
+        prediction = 'Mine'
+    else:
+        prediction = 'Rock'
     
 st.success(prediction)
