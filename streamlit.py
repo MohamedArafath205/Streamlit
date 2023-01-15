11d3b3c753553641f6644870863051e890faa4a8
Mohamed Arafath <113768609+MohamedArafath205@users.noreply.github.com>
2023-01-15 18:13:09 +0530

    Initial commit

---
+++ streamlit.py
@@ -0,0 +1,25 @@
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+# loading the models
+model = pickle.load(open('model.sav', 'rb'))
+
+# Title of the page
+st.title('Rock or Mine Prediction')
+
+input_ = st.text_input("Enter the values ")
+
+final = ''
+
+input_np = input_.resize(1,-1)
+
+if st.button('Predict'):
+    model_predict = model.predict(input_np)
+    
+    if (model_predict[0]=='M'):
+        prediction = 'Mine'
+    else :
+        prediction = 'Rock'
+
+st.success(prediction)
